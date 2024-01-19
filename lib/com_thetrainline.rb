575a3f2139e5b0a48f7da1e94e40c77393499146
Jessica Été <kohrVid@tutanota.com>
2024-01-19 04:24:36 +0100

    Add the Faraday gem for use in the ComTrainline class

--- lib/com_thetrainline.rb
+++ lib/com_thetrainline.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
+require 'date'
+require 'faraday'
+
 class ComThetrainline
   class << self
     def find(from, to, departure_at)
+      conn = Faraday.new(
+        url: 'http://localhost:9000',
+        headers: { 'Content-Type' => 'application/json' }
+      )
+
+      response = conn.post('api/journey-search') do |req|
+        req.body = { query: '' }.to_json
+      end
+
+      puts response.body
+
       []
     end
   end
